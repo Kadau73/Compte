b1859eeb73a1b8787563e9f8a0d0603dd90079a2
Unknown <karl_dau@hotmail.com>
2018-12-18 19:00:06 -0500

    Démo

---
+++ PPTaxe/Calcule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTaxe
+{
+    class Calcule
+    {
+        /// <summary>
+        /// Additionne 2 nombre string
+        /// </summary>
+        /// <param name="Nombre1">Premier Nombre</param>
+        /// <param name="Nombre2">Deuxième Nombre</param>
+        /// <returns></returns>
+        public int Addition2TextBox(string Nombre1, string Nombre2)
+        {
+            int Reponse = 0;
+            Reponse = int.Parse(Nombre1) + int.Parse(Nombre2);
+            return Reponse;
+        }
+
+        /// <summary>
+        /// Multiplie la taxe TPS a un montant donner
+        /// </summary>
+        /// <param name="Montant"></param>
+        /// <returns></returns>
+        public decimal CalculerTPS(string Montant)
+        {
+            decimal tps = 0;
+
+            tps = decimal.Parse(Montant);
+
+            tps = tps * 0.1m;
+
+            return tps;
+        }
+        /// <summary>
+        /// Multiplie la taxe TVQ a un montant donner
+        /// </summary>
+        /// <param name="Montant"></param>
+        /// <returns></returns>
+        public decimal CalculerTVQ(string Montant)
+        {
+            decimal tvq = 0m;
+
+            tvq = decimal.Parse(Montant);
+
+            tvq = tvq * 0.05m;
+
+            return tvq;
+        }
+    }
+}
---
+++ PPTaxe/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPTaxe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Champ
+        private Calcule _calcule = new Calcule();
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddition_Click(object sender, RoutedEventArgs e)
+        {
+            txtReponse.Clear();
+
+            txtReponse.Text += _calcule.Addition2TextBox(txtNombre1.Text, txtNombre2.Text).ToString();
+        }
+
+        private void btnTaxe_Click(object sender, RoutedEventArgs e)
+        {
+            txtTPS.Clear();
+            txtTVQ.Clear();
+
+            txtTPS.Text += _calcule.CalculerTPS(txtMontant.Text).ToString();
+            txtTVQ.Text += _calcule.CalculerTVQ(txtMontant.Text).ToString();
+        }
+    }
+}
